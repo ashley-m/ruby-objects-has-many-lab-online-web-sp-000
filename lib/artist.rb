4e0cac9abb6eb21afe9c13da7347b0b75113a4d8
Ashley Miller <asm2fe@virginia.edu>
2019-04-13 19:17:59 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  attr_accessor :name
+
+  @@songs = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_song(song)
+    @@songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    s = Song.new(name)
+    s.artist = self
+    @@songs << s
+  end
+
+  def self.song_count
+    @@songs.length
+  end
+end
